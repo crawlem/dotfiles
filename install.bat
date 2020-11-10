39e10baafd91451f2ef3eee64e955442fc82bd73
Mark <mark@crawleyweb.co.uk>
2020-11-10 09:49:35 +0000

    Enable WSL

--- install.bat
+++ install.bat
@@ -1,2 +1,3 @@
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 powershell.exe -ExecutionPolicy Unrestricted -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 for /F "eol=#" %%G in (packages.txt) do choco upgrade %%G -fy
