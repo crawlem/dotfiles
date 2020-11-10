9c329a3c2530ff60a73d2dfe4c52b3d87c7f993b
Mark Crawley <mark@crawleyweb.co.uk>
2020-11-10 02:44:36 -0800

    Enable WSL

--- install.bat
+++ install.bat
@@ -1,3 +1,7 @@
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 powershell.exe -ExecutionPolicy Unrestricted -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 for /F "eol=#" %%G in (packages.txt) do choco upgrade %%G -fy
+
+REM https://docs.microsoft.com/en-us/windows/wsl/install-win10
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+wsl --set-default-version 2
