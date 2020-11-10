2537667e48799dd5ac0542629b33bc6dfc6d223a
Mark Crawley <mark.crawley@gmail.com>
2020-11-10 09:18:57 +0000

    Simplified Windows install

--- install.bat
+++ install.bat
@@ -1,1 +1,2 @@
+powershell.exe -ExecutionPolicy Unrestricted -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 for /F "eol=#" %%G in (packages.txt) do choco upgrade %%G -fy
