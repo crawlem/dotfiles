62c67dafbaf6d33854237766174f0b54384b7955
Mark Crawley <mark@crawleyweb.co.uk>
2020-11-11 10:10:39 +0000

    Initial WSL installer

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+# WSL Ubuntu install script
+# -------------------------
+#
+
+# Default SSH key
+if [[ ! -f ~/.ssh/id_ed25519 ]]
+then
+    ssh-keygen -t ed25519
+fi
+
+# Applications
+sudo apt update
+sudo apt upgrade
+sudo apt install nodejs npm
+
+# Dotfiles
+ln -sv “~/dotfiles/git/.gitconfig” ~
