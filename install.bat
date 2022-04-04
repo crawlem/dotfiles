0c284efff456329928f2667e0765756b7d9129cc
Mark Crawley <mark@crawleyweb.co.uk>
2022-04-04 16:08:47 +0100

    Wait for install before config

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 REM Enable local scripts, then execute our PowerShell
 PowerShell Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-PowerShell -command "Start-Process powershell '%cd%\windows\install.ps1 -profile %1' -Verb RunAs"
+PowerShell -command "Start-Process powershell '%cd%\windows\install.ps1 -profile %1' -Verb RunAs -Wait"
 PowerShell -command "Start-Process powershell '%cd%\windows\configure.ps1 -profile %1'"
