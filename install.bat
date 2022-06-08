19b5db7fd7823656b84840fb64dd942e4e697bd8
Mark Crawley <mark@crawleyweb.co.uk>
2022-06-08 12:53:23 +0100

    Don't run locale setup

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 REM Enable local scripts, then execute our PowerShell
 PowerShell Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-PowerShell -command "Start-Process powershell '%cd%\windows\setup.ps1' -Verb RunAs -Wait"
+REM PowerShell -command "Start-Process powershell '%cd%\windows\setup.ps1' -Verb RunAs -Wait"
 PowerShell -command "Start-Process powershell '%cd%\windows\install.ps1 -profile %1' -Verb RunAs -Wait"
 PowerShell -command "Start-Process powershell '%cd%\windows\configure.ps1 -profile %1' -Wait"
